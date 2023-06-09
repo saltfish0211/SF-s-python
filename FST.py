5967a296820a11865cb3bfc59e57238da4898b84
saltfish0211 <yujimtw@gmail.com>
2023-06-09 22:35:37 +0800

    修改計算方式

--- FST.py
+++ FST.py
@@ -6,40 +6,43 @@ import glob,os
 
 # 讀取所有xlsx文件
 time = 15 #!視頻時間（分鐘）
+fluidspeed = 45 #!速度閾值
+sec = 30 #!幀數
 path = r"D:\ps\NA04" # 這裡替換為你的xlsx文件的路徑
 file = glob.glob(os.path.join(path,"*.csv"))
 dl = []
 for f in file:
     dl.append(pd.read_csv(f,header=2,usecols=["coords","x", "y"]))
 
-print(dl)
-# 合併所有數據
-all_data = pd.concat(dl, ignore_index=True)
+for d in range(len(dl)):
+    data1 = dl[d]
+    id = data1['coords']
+    x = data1['x']
+    y = data1['y']
+    MovingTime = 0
+    is_moving = False
+    for i in range(len(id) - 30):
+        for j in range(30):#!這邊是判定一秒的總移動距離
+            j1 = i+j
+            distance_sec = 0
+            plot1 = np.array([x[i],y[i]])
+            plot2 = np.array([x[j1+1],y[j1+1]])
+            distance = np.linalg.norm(plot1-plot2)
+            distance_sec = distance + distance_sec
+        #!以下是判斷這一秒移動距離有沒有超過閾值
+        if distance_sec > fluidspeed and not is_moving: # 開始移動
+            is_moving = True
+            start_time = i
+        elif distance_sec <= fluidspeed and is_moving: # 停止移動
+            is_moving = False
+            end_time = i 
+            MovingTime_sec = (end_time - start_time)/30
+            MovingTime = MovingTime + MovingTime_sec
+    # 如果動物在最後一個數據點仍在移動，我們需要把這段時間也計算在內
+    if is_moving:
+        MovingTime_sec = (len(id)-start_time)/30
+        MovingTime = MovingTime + MovingTime_sec
+    ImortalTime = (time * 60) - MovingTime 
+    print(file[d]+"的靜止是時間為*"+str(ImortalTime)+"sec")
 
-# 設定閾值
-threshold = 1 # 這個閾值需要你根據實際情況設置，也就是動物在水中由於水的流動而產生的輕微浮動
 
-# 計算動物的移動距離並判斷是否超過閾值
-moving_periods = []
-is_moving = False
-for i in range(len(all_data) - 30):
-    dx = all_data.iloc[i+30]['x'] - all_data.iloc[i]['x']
-    dy = all_data.iloc[i+30]['y'] - all_data.iloc[i]['y']
-    distance = np.sqrt(dx**2 + dy**2)
-    
-    if distance > threshold and not is_moving: # 開始移動
-        is_moving = True
-        start_time = i / 30
-    elif distance <= threshold and is_moving: # 停止移動
-        is_moving = False
-        end_time = i / 30
-        moving_periods.append((start_time, end_time))
-
-# 如果動物在最後一個數據點仍在移動，我們需要把這段時間也計算在內
-if is_moving:
-    moving_periods.append((start_time, len(all_data) / 30))
-
-# 計算總的移動時間
-total_moving_time = sum(end - start for start, end in moving_periods)
-total_immortal_time = time * 60 - total_moving_time
-print(f"動物的移動時間為 {total_moving_time} 秒，靜止移動時間為 {total_immortal_time} 秒")
